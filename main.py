83dfbd58f86f74be3dbfeebf6db735f18180fb5a
Ryan Moodie <ryanmoodie@gmail.com>
2024-02-05 23:05:06 +0000

    whitespace

--- main.py
+++ main.py
@@ -147,15 +147,15 @@ class GameState:
                 for j, occupier in enumerate(row):
                     if occupier != BLANK:
                         for direction in HALF_DIRECTIONS:
-                                n = self.count_line_length(i, j, *direction, occupier)
+                            n = self.count_line_length(i, j, *direction, occupier)
 
-                                if n > 1:
-                                    if DEBUG:
-                                        print(
-                                            f"counting {occupier} on {(i,j)} going {direction}: {n}"
-                                        )
+                            if n > 1:
+                                if DEBUG:
+                                    print(
+                                        f"counting {occupier} on {(i,j)} going {direction}: {n}"
+                                    )
 
-                                    score += n if occupier == self.player else -n
+                                score += n if occupier == self.player else -n
 
             if DEBUG:
                 self.print_board()
